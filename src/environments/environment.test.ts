b1a3c527f449b7875572e7f28264ecfbcc5aa088
VioletQQY <forviolet771@gmail.com>
2017-09-18 13:53:40 +0800

    change env

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -4,5 +4,5 @@ export const environment = {
 
 export const app = {
   title: '昌平健康云医院后台',
-  api_url: 'http://10.1.64.194/changping-internal'
+  api_url: 'http://10.1.64.195/changping-internal'
 };
