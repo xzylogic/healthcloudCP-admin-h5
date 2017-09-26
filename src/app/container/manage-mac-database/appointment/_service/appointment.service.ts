7d27ba11051a75eac7591a81255bab0c432e78b8
VioletQQY <forviolet771@gmail.com>
2017-09-26 17:48:41 +0800

    123

--- src/app/container/manage-mac-database/appointment/_service/appointment.service.ts
+++ src/app/container/manage-mac-database/appointment/_service/appointment.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ContainerConfig } from '../../../../libs/common/container/container.component';
+import { ControlType, TableTitle } from '../../../../libs/dtable/dtable.entity';
+
+const PATH = {
+  getData: '/api/motherAndChildFile/checkMotherAndChildFileList',
+  getCommunityAll: '/api/getAllCommunityByUserId',
+  getDetail: '/api/motherAndChildFile/save',
+  saveDetail: '/api/planImmunization/updateInfo'
+};
 
 @Injectable()
 export class AppointmentService {
@@ -13,5 +21,57 @@ export class AppointmentService {
       ifHome: true,
       homeRouter: '/mac-database/appointment'
     });
+  }
+
+  setAppointmentDetailConfig() {
+    return new ContainerConfig({
+      title: '母子建档预约',
+      subTitle: '预约详情',
+      ifHome: false,
+      homeRouter: '/mac-database/appointment'
+    });
+  }
+
+  setAppointmentTitles() {
+    return [
+      new TableTitle({
+        name: '序号',
+        key: '',
+        controlType: ControlType.index
+      }),
+      new TableTitle({
+        name: '姓名',
+        key: 'name'
+      }),
+      new TableTitle({
+        name: '联系电话',
+        key: 'motherContactNumber'
+      }),
+      new TableTitle({
+        name: '预约时间',
+        key: 'reservationDate'
+      }),
+      new TableTitle({
+        name: '预约单号',
+        key: 'motherChildFileNum'
+      }),
+      new TableTitle({
+        name: '所属机构',
+        key: 'hospitalName'
+      }),
+      new TableTitle({
+        name: '订单状态',
+        key: 'statusName'
+      }),
+      new TableTitle({
+        name: '操作',
+        key: '',
+        controlType: ControlType.buttons,
+        option: [{
+          key: 'edit',
+          name: ''
+        }]
+      }),
+    ];
   }
 }
