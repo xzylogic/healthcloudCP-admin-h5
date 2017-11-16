b065d5fbe86767871d20f0a073db6ca2b01a3275
VioletQQY <forviolet771@gmail.com>
2017-11-16 10:10:49 +0800

    modify title text

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,7 +3,7 @@ export const environment = {
 };
 
 export const app = {
-  title: '北京昌平健康云管理后台',
+  title: '北京昌平健康云管理平台',
   api_url: 'http://10.1.64.194/changping-internal',
   base: '/healthcloud-cp-admin/'
 };
