c6a43d8197fa949c2c4c7afe6c7680c8bde14ed9
VioletQQY <forviolet771@gmail.com>
2017-12-20 11:34:43 +0800

    change prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
 
 export const app = {
   title: '北京昌平健康云管理平台',
-  api_url: 'http://10.1.64.194/changping-internal',
+  api_url: 'http://61.50.129.55/changping-internal',
   base: '/healthcloud-cp-admin/'
 };
