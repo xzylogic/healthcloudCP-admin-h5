c2d8311468f79049c1becbf4e7b89d6f9bf15853
VioletQQY <forviolet771@gmail.com>
2018-10-08 17:21:44 +0800

    add vaccine management

--- src/app/libs/dform/component/lib-input/lib-input-checkbox.ts
+++ src/app/libs/dform/component/lib-input/lib-input-checkbox.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild, OnChanges } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 import { FormCheckbox } from '../../_entity/form-checkbox';
@@ -10,11 +10,13 @@ import { FormCheckbox } from '../../_entity/form-checkbox';
       <div class="input_container">
         <section calss="input_content">
           <mat-checkbox class="check_content"
+                      [(checked)]="checkedAll"
+                      (change)="getCheckedAll($event)"
+          >全选</mat-checkbox><mat-checkbox class="check_content"
                        *ngFor="let opt of data.options"
                        [(checked)]="opt.checked"
-                       (change)="getChecked($event, opt.id)"
-          >{{opt.name}}
-          </mat-checkbox>
+                       (change)="getChecked($event, opt.id, opt)"
+          >{{opt.name}}</mat-checkbox>
         </section>
         <span class="input_span">{{data.label}}</span>
         <input type="hidden" [formControlName]="data.key" [(ngModel)]="value" (change)="change()">
@@ -23,7 +25,7 @@ import { FormCheckbox } from '../../_entity/form-checkbox';
   `,
   styleUrls: ['./lib-input.scss']
 })
-export class LibInputCheckboxComponent implements OnInit {
+export class LibInputCheckboxComponent implements OnInit, OnChanges {
   @Input() form: FormGroup;
   @Input() data: FormCheckbox;
   @Input() value: Array<any>;
@@ -31,20 +33,58 @@ export class LibInputCheckboxComponent implements OnInit {
 
   @ViewChild('date') date: any;
 
+  checkedAll = false;
+
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
     this.data.options.forEach(obj => {
+      obj.checked = false;
       if (this.value.indexOf(obj.id) > -1) {
         obj.checked = true;
       }
     });
+    this.setCheckedAll();
   }
 
-  getChecked(opt, id) {
+  setCheckedAll() {
+    const list = [];
+    this.data.options.forEach(obj => {
+      if (obj.checked == true) {
+        list.push(obj);
+      }
+    });
+    console.log(this.data.options);
+    console.log(list);
+    if (list.length === this.data.options.length) {
+      this.checkedAll = true;
+    } else {
+      this.checkedAll = false;
+    }
+    console.log(this.checkedAll);
+  }
+
+  getCheckedAll(event) {
+    const value = [];
+    const checked = event.checked;
+    this.checkedAll = event.checked;
+    this.data.options.forEach(obj => {
+      obj.checked = checked;
+      if (checked) {
+        value.push(obj.id);
+      }
+    });
+    this.value = value;
+  }
+
+  getChecked(event, id, opt) {
+    opt.checked = event.checked;
     let index = this.value.indexOf(id);
-    if (opt.checked) {
+    if (event.checked) {
       if (index = -1) {
         this.value.push(id);
       }
@@ -53,9 +93,12 @@ export class LibInputCheckboxComponent implements OnInit {
         this.value.splice(index, 1);
       }
     }
+    this.change();
+    this.setCheckedAll();
   }
 
   change() {
+    console.log(this.value);
     this.valueChange.emit(this.value);
   }
 }
