f858b7457af83ce662906a4a1baeac4e9f2b1233
VioletQQY <forviolet771@gmail.com>
2018-11-30 11:10:09 +0800

    modify age period

--- src/app/container/manage-pe-for-children/plan/ageperiod.component.ts
+++ src/app/container/manage-pe-for-children/plan/ageperiod.component.ts
@@ -29,8 +29,8 @@ export class AgePeriodComponent implements OnInit, OnChanges {
   ngOnInit() {
     this.planService.getVaccine().subscribe(res => {
       if (res && res.code == 0 && res.data && res.data.content) {
-        this.vaccineList = res.data.content.filter(obj => obj.type == 1);
-        this.vaccineList2 = res.data.content.filter(obj => obj.type == 2);
+        this.vaccineList = res.data.content.filter(obj => obj && obj.type == 1);
+        this.vaccineList2 = res.data.content.filter(obj => obj && obj.type == 2);
       }
     });
   }
@@ -40,16 +40,16 @@ export class AgePeriodComponent implements OnInit, OnChanges {
   }
 
   getDefaultShow(monthAges, type) {
-    const name = monthAges.filter(obj => obj.type == type).map(obj => obj.name);
-    // const nameall = name.filter(n => n === '全月龄');
-    return name.join(',');
+    const name = monthAges.filter(obj => obj && obj.type == type).map(obj => obj && obj.name);
+    const nameall = name.filter(n => n === '全月龄');
+    return nameall.length !== 0 ? nameall[0] : name.join(',');
   }
 
   editVaccine(week, ampm, monthAges) {
     let defalutValue1 = [];
     let defalutValue2 = [];
-    defalutValue1 = monthAges.filter(obj => obj.type == 1).map(data => data.id);
-    defalutValue2 = monthAges.filter(obj => obj.type == 2).map(data => data.id);
+    defalutValue1 = monthAges.filter(obj => obj && obj.type == 1).map(data => data && data.id);
+    defalutValue2 = monthAges.filter(obj => obj && obj.type == 2).map(data => data && data.id);
     const option: DialogEdit = new DialogEdit({
       title: `维护${this.renderWeek(week)}${this.renderAmPm(ampm)}的体检`,
       form: [
@@ -57,17 +57,17 @@ export class AgePeriodComponent implements OnInit, OnChanges {
           key: 'local' + week + ampm,
           label: '本地儿童体检时段',
           value: defalutValue1,
-          allCheckedName: '全选',
-          options: this.vaccineList
-          // options: this.vaccineList.filter(obj => obj.name !== '全月龄')
+          allCheckedName: '全月龄',
+          // options: this.vaccineList
+          options: this.vaccineList.filter(obj => obj && obj.name !== '全月龄')
         }),
         new FormCheckbox({
           key: 'nonlocal' + week + ampm,
           label: '外地儿童体检时段',
           value: defalutValue2,
-          allCheckedName: '全选',
-          options: this.vaccineList2
-          // options: this.vaccineList2.filter(obj => obj.name !== '全月龄')
+          allCheckedName: '全月龄',
+          // options: this.vaccineList2
+          options: this.vaccineList2.filter(obj => obj && obj.name !== '全月龄')
         }),
       ]
     });
@@ -75,10 +75,8 @@ export class AgePeriodComponent implements OnInit, OnChanges {
     const editDialog = EditDialog(option, this.dialog);
     editDialog.afterClosed().subscribe(data => {
       // console.log(data);
-      if (data && data['local' + week + ampm] && data['local' + week + ampm].length === 0) {
-        HintDialog('请选择本地儿童体检时段', this.dialog);
-      } else if (data && data['nonlocal' + week + ampm] && data['nonlocal' + week + ampm].length === 0) {
-        HintDialog('请选择外地儿童体检时段', this.dialog);
+      if (data && !((data['local' + week + ampm] && data['local' + week + ampm].length !== 0) || (data['nonlocal' + week + ampm] && data['nonlocal' + week + ampm].length !== 0))) {
+        HintDialog('请选择儿童体检时段', this.dialog);
       } else if (data && data['local' + week + ampm] && data['nonlocal' + week + ampm]) {
         this.saveVaccine(week, ampm, data['local' + week + ampm], data['nonlocal' + week + ampm]);
       }
@@ -89,16 +87,16 @@ export class AgePeriodComponent implements OnInit, OnChanges {
     // console.log(ids);
     let vaccineId1 = '';
     let vaccineId2 = '';
-    // if ((this.vaccineList.length - 1) === ids.length) {
-    //   vaccineId1 = this.vaccineList.map(obj => obj.id).join(',');
-    // } else {
+    if ((this.vaccineList.length - 1) === ids.length) {
+      vaccineId1 = this.vaccineList.map(obj => obj && obj.id).join(',');
+    } else {
       vaccineId1 = ids.join(',');
-    // }
-    // if ((this.vaccineList2.length - 1) === ids2.length) {
-    //   vaccineId2 = this.vaccineList2.map(obj => obj.id).join(',');
-    // } else {
+    }
+    if ((this.vaccineList2.length - 1) === ids2.length) {
+      vaccineId2 = this.vaccineList2.map(obj => obj && obj.id).join(',');
+    } else {
       vaccineId2 = ids2.join(',');
-    // }
+    }
     const saveData = [{
       week: week,
       amPm: ampm,
